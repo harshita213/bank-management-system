aa4f108e77fa280d6ce9a9b57246950511e4f635
harshita213 <114397897+harshita213@users.noreply.github.com>
2023-11-19 04:18:25 +0530

    bank management systemv

---
+++ src/bank/management/system/Conn.java
@@ -0,0 +1,20 @@
+package bank.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "career123");
+            s = c.createStatement();
+            
+        }catch(Exception e){  
+            
+        }
+    }
+}
+
+
